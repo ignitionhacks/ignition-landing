3b7e8e66138e227fa4c8e77cf07e24fa0dbab789
ylabidi11 <youssef.labidi35@gmail.com>
2026-07-31 18:38:27 -0400

    change dates

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -53,7 +53,7 @@ export default function Hero() {
           Arts & Technology <br />
           Hackathon
         </h1>
-        <p>August 2026</p>
+        <p>August 21-22</p>
         <button onClick={redirectToPortal}>Apply Now!</button>
       </div>
 
